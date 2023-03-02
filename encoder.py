e4ef8d7fb1b1c38513cccb5d71f9d0e8f1450d32
LUGZSPORT0 <arjean85@gmail.com>
2023-03-01 21:53:22 -0500

    add encoder file with encode function

---
+++ encoder.py
@@ -0,0 +1,44 @@
+"""
+Allen Jean encode code
+"""
+
+def encode(password):
+    list_password = []
+    for i in password:
+        list_password.append(int(i) + 3)
+
+    str_password = ""
+    for j in list_password:
+        str_password += str(j)
+
+    return str_password
+
+def menu():
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+
+def main():
+    print()
+    menu()
+    print()
+    user_option = int(input("Please enter an option: "))
+    while user_option != 3:
+        if user_option == 1:
+            orig_pwd = input("Please enter your password to encode: ")
+            encoded = encode(orig_pwd)
+            print("Your password has been encoded and stored!")
+        elif user_option == 2:
+            decoder = decode(encoded)
+            print(f"The encoded password is {encoded}, and the original password is {decoder}")
+        else:
+            break
+        print()
+        menu()
+        user_option = int(input("Please enter an option: "))
+
+if __name__ == "__main__":
+    main()
+
